2887d1181e2ad403a169a0b986bb8eca83143ced
Ananya Agarwal <80957726+ananya-codeit@users.noreply.github.com>
2021-10-05 13:43:24 +0530

    abc

--- chat_app/lib/Screens/mainpage.dart
+++ chat_app/lib/Screens/mainpage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:chat_app/Widgets/chatRow.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -77,39 +78,3 @@ class MainPage extends StatelessWidget {
   }
 }
 
-class ChatRow extends StatelessWidget {
-  const ChatRow({
-    Key? key,
-    this.nickname,this.aboutMe
-  }) : super(key: key);
-  final nickname;
-  final aboutMe;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-         color: Colors.grey.shade300,
-      ),
-     child: Row(
-          children: [
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: CircleAvatar(
-            backgroundColor: Colors.blueGrey,
-          ),
-        ),
-        Column(children: [
-          SizedBox(
-            child: Text(nickname),
-          ),
-          Text(aboutMe),
-        ],)
-        
-      ],
-       
-     ),
-    );
-  }
-}
