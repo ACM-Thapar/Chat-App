add115b0eb9cf5ba973a2d995a87aaab51a01288
Ananya Agarwal <80957726+ananya-codeit@users.noreply.github.com>
2021-10-05 13:32:38 +0530

    work in progress

--- chat_app/lib/Screens/mainpage.dart
+++ chat_app/lib/Screens/mainpage.dart
@@ -49,18 +49,70 @@ class MainPage extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-          Container(
-           
-            height: 60,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-               color: Colors.grey.shade300,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ChatRow(
+              nickname: 'Nickname:ChiPu',
+              aboutMe: 'AboutMe:Shopping',
             ),
-            
-          )
+          ),
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: ChatRow(
+              nickname: 'Nickname:SongTungMTP',
+              aboutMe: 'AboutMe:Like Travel',
+          ),
+           ),
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: ChatRow(
+              nickname: 'Nickname:DangTroung',
+              aboutMe: 'AboutMe:Sing my Song',
+          ),
+           )
         ],
         ),
       )
+    );
+  }
+}
+
+class ChatRow extends StatelessWidget {
+  const ChatRow({
+    Key? key,
+    this.nickname,this.aboutMe
+  }) : super(key: key);
+  final nickname;
+  final aboutMe;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+         color: Colors.grey.shade300,
+      ),
+     child: Row(
+          children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundColor: Colors.blueGrey,
+          ),
+        ),
+        Column(children: [
+          SizedBox(
+            child: Text(nickname),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text(aboutMe),
+          ),
+        ],)
+        
+      ],
+       
+     ),
     );
   }
 }
